163b13dd1e8df8e5f2dd1a21b550a9623f20af37
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2018-09-24 20:59:16 -0500

    introducing kotlin, tweaking launcher icon, setting initial styles, tweaking library versions

---
+++ src/main/java/com/nerdery/umbrella/ui/activities/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.nerdery.umbrella.ui.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import com.nerdery.umbrella.R
+
+class SplashActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
